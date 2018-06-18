03f385ac81a12d54e7211b35ed7153b72d7a964f
Ammar <ammar.mian08@gmail.com>
2018-06-18 09:17:04 -0400

    p13: updating parent chld pointers

--- spec/test.rb
+++ spec/test.rb
@@ -52,7 +52,7 @@ RSpec.describe 'database' do
     result = run_script(script)
     expect(result.last(2)).to match_array([
       "db > Executed.",
-      "db > Need to implement updating parent after split"
+      "db > Need to implement splitting internal node"
     ])
   end
 
@@ -212,6 +212,86 @@ RSpec.describe 'database' do
       "\t\t- 13",
       "\t\t- 14",
       "db > Executed.",
+      "db > "
+    ])
+  end
+
+  it 'allows printing out the structure of a 4-leaf-node btree' do
+    script = [
+      "insert 18 user18 person18@example.com",
+      "insert 7 user7 person7@example.com",
+      "insert 10 user10 person10@example.com",
+      "insert 29 user29 person29@example.com",
+      "insert 23 user23 person23@example.com",
+      "insert 4 user4 person4@example.com",
+      "insert 14 user14 person14@example.com",
+      "insert 30 user30 person30@example.com",
+      "insert 15 user15 person15@example.com",
+      "insert 26 user26 person26@example.com",
+      "insert 22 user22 person22@example.com",
+      "insert 19 user19 person19@example.com",
+      "insert 2 user2 person2@example.com",
+      "insert 1 user1 person1@example.com",
+      "insert 21 user21 person21@example.com",
+      "insert 11 user11 person11@example.com",
+      "insert 6 user6 person6@example.com",
+      "insert 20 user20 person20@example.com",
+      "insert 5 user5 person5@example.com",
+      "insert 8 user8 person8@example.com",
+      "insert 9 user9 person9@example.com",
+      "insert 3 user3 person3@example.com",
+      "insert 12 user12 person12@example.com",
+      "insert 27 user27 person27@example.com",
+      "insert 17 user17 person17@example.com",
+      "insert 16 user16 person16@example.com",
+      "insert 13 user13 person13@example.com",
+      "insert 24 user24 person24@example.com",
+      "insert 25 user25 person25@example.com",
+      "insert 28 user28 person28@example.com",
+      ".btree",
+      ".exit"
+    ]
+    result = run_script(script)
+    expect(result[30...(result.length)]).to eq([
+      "db > Tree:",
+      "- internal (size 3)",
+      "\t- leaf (size 7)",
+      "\t\t- 1",
+      "\t\t- 2",
+      "\t\t- 3",
+      "\t\t- 4",
+      "\t\t- 5",
+      "\t\t- 6",
+      "\t\t- 7",
+      "- key 7",
+      "\t- leaf (size 8)",
+      "\t\t- 8",
+      "\t\t- 9",
+      "\t\t- 10",
+      "\t\t- 11",
+      "\t\t- 12",
+      "\t\t- 13",
+      "\t\t- 14",
+      "\t\t- 15",
+      "- key 15",
+      "\t- leaf (size 7)",
+      "\t\t- 16",
+      "\t\t- 17",
+      "\t\t- 18",
+      "\t\t- 19",
+      "\t\t- 20",
+      "\t\t- 21",
+      "\t\t- 22",
+      "- key 22",
+      "\t- leaf (size 8)",
+      "\t\t- 23",
+      "\t\t- 24",
+      "\t\t- 25",
+      "\t\t- 26",
+      "\t\t- 27",
+      "\t\t- 28",
+      "\t\t- 29",
+      "\t\t- 30",
       "db > "
     ])
   end
